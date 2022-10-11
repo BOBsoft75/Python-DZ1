4527e9d16a72a27cec4afe96e55ada0b031a0612
BOBsoft75 <bobsoft_spb@mail.ru>
2022-10-11 22:59:14 +0300

    DZ1

--- DZ1_2/DZ1_2.py
+++ DZ1_2/DZ1_2.py
@@ -2,44 +2,10 @@
 # Напишите программу для. проверки истинности утверждения
 #  ¬(X ⋁ Y ⋁ Z) = ¬X ⋀ ¬Y ⋀ ¬Z для всех значений предикат.
 
-x = False
-y = False
-z = False
-result = True
-
-
-def chek(x, y, z):
-    if (not (x or y or z)) == (not x and not y and not z):
-        result = True
-        print(1)
-    else:
-        result = False
-        print(0)
-    return result
-
-
-chek(x, y, z)
-
-# resultLeft = True
-# resultRight = True
-# if x != True:
-#     if y != True:
-#         if z != True:
-#             resultLeft = True
-#             print(1)
-# else:
-#     resultLeft = False
-
-
-# if not x == True:
-#     if not y == True:
-#         if not z == True:
-#             resultRight = False
-#             print(0)
-# else:
-#     resultRight = True
-# if resultLeft == resultRight:
-# if (not (x or y or z)) == (not x and not y and not z):
-#     print('1 ')
-# else:
-#     print('0 ')
+for x in range(2):
+    for y in range(2):
+        for z in range(2):
+            if (not (x or y or z)) == (not x and not y and not z):
+                print(f'{x} {y} {z} Истина')
+            else:
+                print(f'{x} {y} {z} Ложь')
